d47156a7ca3d81222c531c9357140c4ebbc840c1
Fayoz <qahramonovfayozfcb@gmail.com>
2021-09-10 00:03:59 +0500

    I have problems with github(

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,7 +1,12 @@
 import s from './layout.module.css'
 const Layout = (props) =>{
+    const background = {
+        backgroundImage: `url(${props.urlBg})`,
+        backgroundImage2: `url(${props.urlBg})`,
+        backgroundColor: `${props.colorBg}`
+    }
     return(
-        <section className={s.root}>
+        <section className={s.root} style={background}>
             <div className={s.wrapper}>
                 <article>
                     <div className={s.title}>
