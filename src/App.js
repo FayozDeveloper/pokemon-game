90f00e148f580cf101f2c1b17b2e24bc042734a5
Fayoz <qahramonovfayozfcb@gmail.com>
2021-09-09 22:58:34 +0500

    second day marathon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header/header";
+import Layout from "./components/layout/layout";
+import Footer from "./components/footer/footer";
+import firstImg from '../src/image/wp4676574-4k-pc-wallpapers.jpg'
+import secondImg from '../src/image/wp4676582-4k-pc-wallpapers.jpg'
 
 function App() {
+
+    const bgColor = 'red'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header title={'This is title'} desc={'This is Description!'}/>
+        <Layout title={"First Layout"} desc={'First Layout Desc'} urlBg={firstImg} />
+        <Layout colorBg={bgColor} title={'Second Layout'}  desc={'Second Layout Desc'}/>
+        <Layout urlBg={secondImg} title={'Third Layout'}  desc={'Third Layout Desc'}/>
+        <Footer/>
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import s from './footer.module.css'
+const Footer = () =>{
+    return(
+        <footer>
+            <div className={s.wrapper}>
+                <h3>THANKS FOR VISITING</h3>
+                <p>© 2021 #ReactMarathon.</p>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
