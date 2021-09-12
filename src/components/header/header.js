3d88b846434410f5a7c1d8f55e9929db159570e4
Fayoz <qahramonovfayozfcb@gmail.com>
2021-09-12 20:10:04 +0500

    fixed problem

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,7 @@ import s from './header.module.css'
 const Header = (props) =>{
     return(
         <header className={s.root}>
-            <div className={s.forest}></div>
+            <div className={s.forest}>i</div>
             <div className={s.container}>
                 <h1>{props.title}</h1>
                 <p>{props.desc}</p>
